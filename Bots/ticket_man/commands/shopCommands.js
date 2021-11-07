692bd30c9f667cb97e8e06b02be8fc66e892f80a
shkelliton <71197165+shkelliton@users.noreply.github.com>
2021-11-07 14:50:07 -0500

    Prelims
    Moving Files and organizing

---
+++ Bots/ticket_man/commands/shopCommands.js
@@ -0,0 +1,26 @@
+//Commands specifically for shop channels. These should ONLY work in 
+//specified shop channels
+
+//Cart command
+
+//-buy [dino]
+/*buy takes the input and finds it in the directory - List, maybe?
+ for example:[0] tekRex, [1]1000
+ need to make everything 'fit' in the cart and be held in memory for that 
+ specific channel 
+ let total = 0
+ let total = total+item;
+
+
+*/
+//bring up 'What color would you like?' reactions for colors
+//reacting adds to cart
+
+//-remove [dino/item]
+
+//removes item from cart
+
+//-cart
+
+//shows total from all dinos/items in cart along with what is in the cart
+
