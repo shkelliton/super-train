ca51bb8647cba79c04931624590c534f21156ecf
shkelliton <71197165+shkelliton@users.noreply.github.com>
2021-11-08 22:10:13 -0500

    Basic vidplayer html practice for 3f02
    Not much functionality

--- 3F02/yT_Vidplayer_Tut/player.js
+++ 3F02/yT_Vidplayer_Tut/player.js
@@ -1,1 +1,21 @@
 // JavaScript source code
+const myVideo = document.getElementById('myVideo');
+const btnPlay = document.getElementById('btnPlay');
+const timeOut = document.getElementById('timeOut');
+let timer = null;
+
+btnPlay.addEventListener('click', vidAction);
+myVideo.addEventListener('click', vidAction);
+
+//vids
+
+function vidAction(event){
+	switch(event.target.id){
+		case "btnPlay" :
+		playVideo();
+		break;
+	}
+}
+function playVideo(){
+	myVideo.play();
+}
