d560e2329cf971bba3e4d7c934af6c20bb4f13d8
shkelliton <71197165+shkelliton@users.noreply.github.com>
2021-11-11 18:10:19 -0500

    Fixed player hopefully
    Stupid constants

--- 3F02/yT_Vidplayer_Tut/player.js
+++ 3F02/yT_Vidplayer_Tut/player.js
@@ -1,13 +1,20 @@
 // JavaScript source code
+//from https://www.youtube.com/watch?v=kXBs8Jd4_Q0
+
 const myVideo = document.getElementById('myVideo');
 const btnPlay = document.getElementById('btnPlay');
-const timeOut = document.getElementById('timeOut');
+const btnNext = document.getElementById('btnNext');
+
 let timer = null;
 
 btnPlay.addEventListener('click', vidAction);
 myVideo.addEventListener('click', vidAction);
+btnNext.addEventListener('click', nextVideo);
 
 //vids
+
+const vids = ["sample-5s.mp4", "sample-10s.mp4"];
+let vidPlaying = 0;
 
 function vidAction(event){
 	switch(event.target.id){
@@ -20,7 +27,17 @@ function playVideo(){
 	myVideo.play();
 }
 
+function nextVideo(){
+	if (vidPlaying < 1){
+		vidPlaying++;
+	} else {
+		vidPlaying = 0;
+	}
+	myVideo.src = "videos/" + vids[vidPlaying];
+}
+
 //from https://stackoverflow.com/questions/11171781/full-screen-video-toggle-html
+
 function goFullscreen(id) {
   var element = document.getElementById(id);       
   if (element.mozRequestFullScreen) {
